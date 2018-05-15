cfcdef63792ac2893eddfa88ea6bf9ce3b17aeb6
1002237 <1002237@SKP1002237MN001.local>
2018-05-16 02:08:46 +0900

    collection view add for news list

---
+++ client/ios/Wasabi/TextNewsCell.swift
@@ -0,0 +1,19 @@
+//
+//  TextNewsCell.swift
+//  Wasabi
+//
+//  Created by 1002237 on 2018. 5. 12..
+//  Copyright © 2018년 1002237. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+class TextNewsCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    func load(newsItem:JSON){
+        titleLabel.text = newsItem.dictionary?["title"]?.stringValue
+        summaryLabel.text = newsItem.dictionary?["summarize"]?.stringValue
+        
+    }
+}
